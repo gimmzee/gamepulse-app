aeea538cb7b11a945dd25c8ae847d000ab9aafe8
gimmzee <jiyoung2589@gmail.com>
2026-04-01 13:10:35 +0900

    Initial commit: GamePulse Spring Boot app

---
+++ src/main/java/com/gamepulse/GamepulseAppApplication.java
@@ -0,0 +1,20 @@
+package com.gamepulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GamepulseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamepulseAppApplication.class, args);
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "GamePulse is running";
+    }
+}
---
+++ src/test/java/com/gamepulse/GamepulseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamepulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamepulseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
